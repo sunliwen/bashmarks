b0e68a4ce860fdc519c486cbd4926c67bf873218
Sun Liwen <sunliwen@gmail.com>
2013-03-02 11:28:22 +0800

    Add bm prefix to avoid command name conflicts

--- bashmarks.sh
+++ bashmarks.sh
@@ -39,8 +39,8 @@ fi
 touch $SDIRS
 
 # save current directory to bookmarks
-function s {
-    check_help $1
+function bmsave {
+    bmcheck_help $1
     _bookmark_name_valid "$@"
     if [ -z "$exit_message" ]; then
         _purge_line "$SDIRS" "export DIR_$1="
@@ -50,22 +50,22 @@ function s {
 }
 
 # jump to bookmark
-function g {
-    check_help $1
+function bmgo {
+    bmcheck_help $1
     source $SDIRS
     cd "$(eval $(echo echo $(echo \$DIR_$1)))"
 }
 
 # print bookmark
-function p {
-    check_help $1
+function bmp {
+    bmcheck_help $1
     source $SDIRS
     echo "$(eval $(echo echo $(echo \$DIR_$1)))"
 }
 
 # delete bookmark
-function d {
-    check_help $1
+function bmd {
+    bmcheck_help $1
     _bookmark_name_valid "$@"
     if [ -z "$exit_message" ]; then
         _purge_line "$SDIRS" "export DIR_$1="
@@ -74,7 +74,7 @@ function d {
 }
 
 # print out help for the forgetful
-function check_help {
+function bmcheck_help {
     if [ "$1" = "-h" ] || [ "$1" = "-help" ] || [ "$1" = "--help" ] ; then
         echo ''
         echo 's <bookmark_name> - Saves the current directory as "bookmark_name"'
@@ -87,8 +87,8 @@ function check_help {
 }
 
 # list bookmarks with dirnam
-function l {
-    check_help $1
+function bmlist {
+    bmcheck_help $1
     source $SDIRS
         
     # if color output is not working for you, comment out the line below '\033[1;32m' == "red"
